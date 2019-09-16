47ee876389b55b06690851125589a532b5670433
Lucky BUANG <lbuang@c5r6s11.wethinkcode.co.za>
2019-09-16 13:44:06 +0200

    update

--- src/lg_r.c
+++ src/lg_r.c
@@ -6,7 +6,7 @@
 /*   By: lbuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/16 10:09:54 by lbuang            #+#    #+#             */
-/*   Updated: 2019/09/16 10:10:33 by lbuang           ###   ########.fr       */
+/*   Updated: 2019/09/16 13:43:25 by lbuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,7 +91,5 @@ t_files					*ft_list(char *curr_dir, t_flags flags)
 		lists.tmp = lists.tmp->next;
 	}
 	ft_print_r(lists.tmp, flags);
-	// if (flags.lg_r == TRUE)
-	// 	ft_list_b(lists.tmp2, curr_dir, flags);
 	return (lists.files);
 }
